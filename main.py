20f41786a50d89b38f81b21e159e837d516a84dc
goffffhxhrhfuudu-lab <goffffhxhrhfuudu@gmail.com>
2026-06-09 18:32:04 +0300

    Update main.py

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ def main():
     app.add_handler(CommandHandler("help", help_command))
 
     print("🏍️ Бот запущен!")
-    app.run_polling()
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
 
 if __name__ == "__main__":
     main()
